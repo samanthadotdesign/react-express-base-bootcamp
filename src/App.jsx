ed9b314f4b6f815a19139ffde932e432f5855cb0
michelle-mok <michellemokhuihui@gmail.com>
2021-07-07 22:48:35 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  // array to use with map
+  const [board, setBoard] = useState([
+    ['','',''],
+    ['','',''],
+    ['','',''],
+  ]);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+
+  const handleClick = (rowIndex, colIndex) => {
+    let newBoard = [...board];
+    newBoard[rowIndex][colIndex] = currentPlayer;
+    setBoard([...newBoard]);
+    currentPlayer === 'X' ? setCurrentPlayer('O') : setCurrentPlayer('X');
+  }
+
+const rows = board.map((row, rowIndex) => {
+  // row is one *unnested* array
+  console.log('row', row);
+  const buttons = row.map((square, colIndex) => (<button onClick={() => handleClick(rowIndex, colIndex)}>{square}</button>));
   return (
-    <div>
-      This is App.jsx
-    </div>
-  );
+  <div className="boardRow">
+    {buttons}
+  </div>);
+});
+
+return (
+  <div className="board">
+    {rows}
+  </div>
+)
 }
