4ef16fe2529241c8eb3a461ce1bbbaa49b0bb55c
samanthadotdesign <samleeml@gmail.com>
2021-07-14 17:33:16 +0800

    initial commit for saving links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+  const [links, setLinks] = useState([]);
+
+  // Save the value of inputText
+  const handleInputText = (e) => {
+    setInputText(e.target.value);
+  };
+
+  // Save the inputText in the links
+  const handleLink = () => {
+    setLinks([...links, inputText]);
+    // Empty the input text after clicking submit
+    setInputText('');
+  };
+
   return (
     <div>
-      This is App.jsx
+      <div>
+        {/* handleInputText */}
+        <input value={inputText} onChange={handleInputText} />
+        <button onClick={handleLink}>Submit</button>
+      </div>
+      <div>
+        {/* We map every link to a new p tag */}
+        {links.map((link) => (
+          <p>{link}</p>
+        ))}
+      </div>
     </div>
   );
 }
